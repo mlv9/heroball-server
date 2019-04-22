a6199a46e2809e10c7de6a43ff50a709ab5aa7f2
Matt <readspaceapp@gmail.com>
2019-04-23 09:44:17 +1000

    Expanded the dummy data

--- db/insert_dummy_data.sql
+++ db/insert_dummy_data.sql
@@ -11,6 +11,8 @@ INSERT INTO Locations (Name) VALUES
 
 INSERT INTO Players (Name, Position, Email, YearStarted, Description) VALUES
     ('Steven', 'center', 'something2@email.com', 1999, 'Some Description For Steven'),
+    ('Jake', 'point-guard', 'something3@email.com', 2007, 'Some Description For Jake'),
+    ('Andrew', 'small-forward', 'something4@email.com', 2012, 'Some Description For Andrew'),
     ('Matthew', 'guard', 'something@email.com', 1998, 'Some Description');
 
 INSERT INTO Stats (
@@ -30,7 +32,11 @@ INSERT INTO Stats (
     RegularFoulsCommitted,
     TechnicalFoulsCommitted,
     MinutesPlayed
-) VALUES (5,10,1,3,2,2,2,6,2,1,1,3,2,4,0,29);
+) VALUES 
+    (5,10,1,3,2,2,2,6,2,1,1,3,2,4,0,29),
+    (3,11,2,4,2,4,2,6,2,2,2,1,4,2,1,35),
+    (1,4,0,3,0,0,2,6,0,0,0,2,1,0,0,40),
+    (2,10,0,0,1,2,3,4,5,2,4,3,4,2,1,21);
 
 INSERT INTO Games (
     CompetitionId, 
@@ -41,4 +47,8 @@ INSERT INTO Games (
 ) VALUES (1,1,1,2,'2019-3-20'::timestamp);
 
 INSERT INTO PlayerGames (PlayerId, GameId, TeamId, StatsId, JerseyNumber)
-    VALUES (1, 1, 1, 1, 9);
+    VALUES 
+        (1, 1, 1, 1, 9),
+        (2, 1, 1, 2, 8),
+        (3, 1, 2, 3, 11),
+        (4, 1, 2, 4, 2);
