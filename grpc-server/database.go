c48fa37e1d5d1fada4e88f02c0b22df4025c6889
Matt <readspaceapp@gmail.com>
2019-04-25 01:13:24 +1000

    Reduced stats leader min games to 1

--- grpc-server/database.go
+++ grpc-server/database.go
@@ -137,7 +137,7 @@ func (database *HeroBallDatabase) GetCompetitionInfo(competitionId int32) (*pb.C
 
 	compInfo.Standings = standings
 
-	statLeaders, err := database.getCompetitionStatsLeaders(competitionId, 3)
+	statLeaders, err := database.getCompetitionStatsLeaders(competitionId, 1)
 
 	if err != nil {
 		return nil, err
