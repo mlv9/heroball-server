7203ceb9fc526087a2062fd01f66c3ba8641df25
Matt <readspaceapp@gmail.com>
2019-05-13 12:07:28 +1000

    and again

--- grpc-server/database.go
+++ grpc-server/database.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/lib/pq"
 	_ "github.com/lib/pq"
@@ -298,7 +299,7 @@ func (database *HeroBallDatabase) GetGamesCursor(offset int32, count int32, filt
 
 	var totalGames int32
 
-	fmt.Printf("PlayerArray: %v", pq.Array(filter.GetPlayerIds()))
+	log.Printf("PlayerArray: %v", pq.Array(filter.GetPlayerIds()))
 
 	/* get the count - potentially expensive for each cursor page... */
 	err := database.db.QueryRow(`
